a66ebf3bc09661fc4c214a8a15a667a7870d3e0d
Oleksandr Tara <rezikun@gmail.com>
2020-04-18 12:36:10 +0300

    first working page

--- .vscode/json.go
+++ .vscode/json.go
@@ -1,23 +1,23 @@
-package covid
+package main
 
 type TimelineJSON struct {
 	Locations []Location
 }
 
 type ActivityWithConfidenceJSON struct {
-	Type string
-	Confidence  int
+	Type       string
+	Confidence int
 }
 
 type ActivityJSON struct {
-	TimestampMs int64
+	TimestampMs                int64
 	ActivityWithConfidenceJSON []ActivityWithConfidenceJSON
 }
 
 type Location struct {
-	Timestamp int64
-	LatitudeE7 int
+	Timestamp   int64
+	LatitudeE7  int
 	LongitudeE7 int
-	Accuracy int
-	Activity []ActivityJSON
+	Accuracy    int
+	Activity    []ActivityJSON
 }
